f4a1b2c3c5fb5ff58f2de54e13a2e8a1ac205e14
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-07-09 16:39:59 -0400

    metodos notas y tratos

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from flask import request
 from flask import render_template
 from flask_migrate import Migrate
-from models import Contact, User, db
+from models import Contact, Deal, Note, User, db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -17,6 +17,7 @@ Migrate(app, db)
 @app.route('/')
 def main():
     return render_template('index.html')
+
 
 @app.route('/api/users', methods=['GET', 'POST'])
 @app.route('/api/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
@@ -49,6 +50,27 @@ def users(id = None):
         user.save()
 
         return jsonify(user.serialize()), 201
+    elif request.method == 'PUT':
+        name = request.json.get('name')
+        last_name = request.json.get('last_name')
+        rut = request.json.get('rut')
+        phone = request.json.get('phone')
+        email = request.json.get('email')
+
+        user = User()
+        user.name = name
+        user.last_name = last_name
+        user.rut = rut
+        user.phone = phone
+        user.email = email
+
+        user.update()
+
+        return jsonify(user.serialize()), 201
+    elif request.method == 'DELETE':
+        user = User.query.get(id)
+        user.delete()
+        return jsonify({ "success": "User deleted"}), 200
     
 
 @app.route('/api/contact', methods=['GET', 'POST'])
@@ -63,6 +85,7 @@ def users(id = None):
             contacts = Contact.query.all()
             contacts = list(map(lambda contact: contact.serialize(), contacts))
             return jsonify(contacts.serialize()), 200
+
     elif request.method == 'POST':
         name = request.json.get('name')
         last_name = request.json.get('last_name')
@@ -87,6 +110,134 @@ def users(id = None):
 
         return jsonify(contact.serialize()), 201
 
+    elif request.method == 'PUT':
+        name = request.json.get('name')
+        last_name = request.json.get('last_name')
+        rut = request.json.get('rut')
+        type = request.json.get('type')
+        phone = request.json.get('phone')
+        email = request.json.get('email')
+        user_id = request.json.get('user_id')
+
+        contact = Contact()
+        contact.name = name
+        contact.last_name = last_name
+        contact.rut = rut
+        contact.type = type
+        contact.phone = phone
+        contact.email = email
+        contact.user_id = user_id
+
+        contact.update()
+
+        return jsonify(contact.serialize()), 201
+
+    elif request.method == 'DELETE':
+        contact = Contact.query.get(id)
+        contact.delete()
+        return jsonify({ "success": "Contact deleted"}), 200
+
+
+@app.route('/api/notes', methods=['GET', 'POST'])
+@app.route('/api/notes/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def notes(id = None):
+    
+    if request.method == 'GET':
+        if id != None:
+            note = Note.query.get(id)
+            return jsonify(note.serialize()), 200
+        else:
+            notes = Note.query.all()
+            notes = list(map(lambda note: note.serialize(), notes))
+            return jsonify(notes.serialize()), 200
+
+    elif request.method == 'POST':
+        comment = request.json.get('comment')
+        create_at = datetime.datetime.now()
+        user_id = request.json.get('user_id')
+        contact_id = request.json.get('contact_id')
+
+        note = Note()
+        note.comment = comment
+        note.create_at = create_at
+        note.user_id = user_id
+        note.contact_id = contact_id
+
+        note.save()
+
+        return jsonify(note.serialize()), 201
+
+    elif request.method == 'PUT':
+        comment = request.json.get('comment')
+        user_id = request.json.get('user_id')
+        contact_id = request.json.get('contact_id')
+
+        note = Note()
+        note.comment = comment
+        note.user_id = user_id
+        note.contact_id = contact_id
+
+        note.update()
+
+        return jsonify(note.serialize()), 201
+
+    elif request.method == 'DELETE':
+        note = Note.query.get(id)
+        note.delete()
+        return jsonify({ "success": "Note deleted"}), 200
+
+
+@app.route('/api/deals', methods=['GET', 'POST'])
+@app.route('/api/deals/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def deals(id = None):
+    
+    if request.method == 'GET':
+        if id != None:
+            deal = Deal.query.get(id)
+            return jsonify(deal.serialize()), 200
+        else:
+            deals = Note.query.all()
+            deals = list(map(lambda deal: deal.serialize(), deals))
+            return jsonify(deals.serialize()), 200
+
+    elif request.method == 'POST':
+        name = request.json.get('name')
+        description = request.json.get('description')
+        create_at = datetime.datetime.now()
+        user_id = request.json.get('user_id')
+        contact_id = request.json.get('contact_id')
+
+        deal = Deal()
+        deal.name = name
+        deal.description = description
+        deal.create_at = create_at
+        deal.user_id = user_id
+        deal.contact_id = contact_id
+
+        deal.save()
+
+        return jsonify(deal.serialize()), 201
+
+    elif request.method == 'PUT':
+        name = request.json.get('name')
+        description = request.json.get('description')
+        user_id = request.json.get('user_id')
+        contact_id = request.json.get('contact_id')
+
+        deal = Deal()
+        deal.name = name
+        deal.description = description
+        deal.user_id = user_id
+        deal.contact_id = contact_id
+
+        deal.update()
+
+        return jsonify(deal.serialize()), 201
+
+    elif request.method == 'DELETE':
+        deal = Deal.query.get(id)
+        deal.delete()
+        return jsonify({ "success": "Deal deleted"}), 200
 
 if __name__ == '__main__':
     app.run()
